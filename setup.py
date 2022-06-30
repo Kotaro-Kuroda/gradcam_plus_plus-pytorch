8b8f028a969eb0d5f3a1c5bd57f26563fa1b0ec0
Kotaro Kuroda <f1004.kuro@gmail.com>
2022-06-30 10:11:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['opencv-python-headless'],  # Optional
+    install_requires=['opencv-python-headless==4.5.3.56'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
